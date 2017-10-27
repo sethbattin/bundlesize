5d4029bb68b058bd84a1e529e2b5b01d88b68cc3
Seth Battin <sbattin@truecar.com>
2017-10-27 12:45:56 -0500

    remove vim-paste accidents

--- src/input.js
+++ src/input.js
@@ -1,8 +1,8 @@
 const input = require('commander')
 
 input
-  .option('-f,_--files_[files]', 'files_to_test_against_(dist/*.js)')
-  .option('-s,_--max-size_[maxSize]', 'maximum_size_threshold_(3Kb)')
+  .option('-f, -files_[files]', 'files_to_test_against_(dist/*.js)')
+  .option('-s, --max-size_[maxSize]', 'maximum_size_threshold_(3Kb)')
   .option('--debug', 'run_in_debug_mode')
 
 module.exports = input
