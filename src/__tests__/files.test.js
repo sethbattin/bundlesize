24cc5cebaedef7741559864159c7496496d0fd0e
Seth Battin <sbattin@truecar.com>
2017-11-01 23:51:41 -0500

    add initial file name generation and partial tests

--- src/__tests__/files.test.js
+++ src/__tests__/files.test.js
@@ -16,7 +16,8 @@ let globMock = null
 
 const mockFileArray = jest.fn()
 jest.mock('../config', () => ({
-  files: { reduce: mockFileArray, name: 'unused' }
+  files: { reduce: mockFileArray },
+  name: 'unused'
 }))
 
 describe('files.js', () => {
@@ -25,29 +26,75 @@ describe('files.js', () => {
     globMock = jest.fn()
     jest.doMock('glob', () => ({ sync: globMock }))
   })
-
-  it('uses config for default arguments', () => {
-    require('../files')()
-    expect(mockFileArray).toBeCalled()
+  describe('cli behavior', () => {
+    it('uses config for default arguments', () => {
+      require('../files')()
+      expect(mockFileArray).toBeCalled()
+    })
   })
+  describe('configurability', () => {
+    // set up test mocks for paths, optionally specifying glob results
+    const filesGlobsResults = (files, globs) => {
+      const _files = files.map((f, i) => {
+        return Object.assign({}, { maxSize: '13B', path: `${i}.js` }, f)
+      })
+      const _globs = globs || _files.map(f => [f.path])
+      _globs.forEach(g => globMock.mockReturnValueOnce(g))
+      const testee = require('../files')(_files)
+      return testee
+    }
 
-  it('returns files from input with size added', () => {
-    const file = { maxSize: '11B', path: 'a.js' }
-    globMock.mockReturnValueOnce(['a.js'])
-    const results = require('../files')([file])
-    expect(results).toHaveLength(1)
-    expect(results[0]).toEqual({ maxSize: 11, path: 'a.js', size: FILE_SIZE })
-  })
-  it('expands glob patterns', () => {
-    const file = { maxSize: '13B', path: 'a-*.js' }
-    globMock.mockReturnValueOnce(['a-0.js', 'a-1.js'])
-    const results = require('../files')([file])
-    expect(results).toHaveLength(2)
-    expect(results).toEqual(
-      expect.arrayContaining([
-        { maxSize: 13, path: 'a-0.js', size: FILE_SIZE },
-        { maxSize: 13, path: 'a-1.js', size: FILE_SIZE }
-      ])
-    )
+    it('returns files from input with size added', () => {
+      const results = filesGlobsResults([{ path: 'a.js' }])
+      expect(results).toHaveLength(1)
+      expect(results[0]).toEqual({
+        maxSize: 13,
+        path: 'a.js',
+        size: FILE_SIZE,
+        name: 'a.js'
+      })
+    })
+    it('expands glob patterns', () => {
+      const results = filesGlobsResults(
+        [{ path: 'a-*.js' }],
+        [['a-0.js', 'a-1.js']]
+      )
+      expect(results).toHaveLength(2)
+      expect(results[0]).toMatchObject({
+        maxSize: 13,
+        path: 'a-0.js',
+        size: FILE_SIZE
+      })
+      expect(results[1]).toMatchObject({
+        maxSize: 13,
+        path: 'a-1.js',
+        size: FILE_SIZE
+      })
+    })
+    describe('name property', () => {
+      it('passes through a string name on a single path', () => {})
+      it('expands a string name with a numeral for expanded paths', () => {})
+      it('checks for duplicate names and errors out', () => {})
+    })
+    describe('replace property', () => {
+      it('creates a name from the path using a string.replace argument pair object', () => {
+        const file = {
+          path: 'long-file-name.js',
+          replace: {
+            pattern: 'long-file',
+            replacement: 'short'
+          }
+        }
+        const results = filesGlobsResults([file])
+        expect(results[0]).toEqual({
+          maxSize: 13,
+          path: 'long-file-name.js',
+          size: FILE_SIZE,
+          name: 'short-name.js'
+        })
+      })
+      it('it accepts Regexes and functions', () => {})
+      it('it converts glob results', () => {})
+    })
   })
 })
--- src/files.js
+++ src/files.js
@@ -5,15 +5,33 @@ const gzip = require('gzip-size')
 const config = require('./config')
 const debug = require('./debug')
 
-const pathName = (path, name) => {
-  return path // TODO: process name
+const getReplacer = opts => {
+  if (opts && opts.pattern && opts.replacement) {
+    const pattern =
+      Array.isArray(opts.pattern) && opts.pattern.length
+        ? new RegExp(opts.pattern[0], opts.pattern[1] || '')
+        : opts.pattern
+
+    return [pattern, opts.replacement]
+  }
 }
 
-const files = (input = config.files, name = config.name) => {
+const pathName = (path, name, i, replacement) => {
+  if (replacement) {
+    return path.replace(replacement[0], replacement[1])
+  } else if (name) {
+    const suffix = i > 0 ? `_${i}` : ''
+    return `${name}${suffix}`
+  }
+  return path
+}
+
+const files = (input = config.files, replace = config.replace) => {
   const files = input.reduce((all, file) => {
-    const paths = glob.sync(file.path).map(expand => {
+    const paths = glob.sync(file.path).map((expand, i) => {
       const size = gzip.sync(fs.readFileSync(expand, 'utf8'))
-      const name = pathName(expand, file.name || name)
+      const replacement = getReplacer(file.replace || replace)
+      const name = pathName(expand, file.name, i, replacement)
       const maxSize = bytes(file.maxSize) || Infinity
       return { path: expand, name, size, maxSize }
     })
