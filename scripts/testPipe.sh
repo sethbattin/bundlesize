c9c757cf3a828a2004d274806f71fa6535f3aab2
Seth Battin <sbattin@truecar.com>
2017-11-02 23:55:38 -0500

    block self tests from publishing data

--- scripts/testPipe.sh
+++ scripts/testPipe.sh
@@ -1,1 +1,6 @@
-cat bin/pipe.js | node bin/pipe --name pipe.js --max-size 1kb
+cat bin/pipe.js | \
+github_token='' \
+GITHUB_TOKEN='' \
+bundlesize_github_token='' \
+BUNDLESIZE_GITHUB_TOKEN='' \
+node bin/pipe --name pipe.js --max-size 1kb
